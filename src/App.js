c7bb39c90cfbc1664fe91edf6de1ad3aef0d2710
DanDean191 <Daniel.Dean191@hotmail.com>
2022-06-05 16:36:46 +0100

    Replaced BrowserRouter with HashRouter to fix Github pages 404 issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route  } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
